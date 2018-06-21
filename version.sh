22ca81c2128c4919818cf51a11e86a571c6be49a
Sébastien Vançon <sebastien.vancon@orange.com>
2018-06-21 14:01:56 +0200

    Update gradle

--- version.sh
+++ version.sh
@@ -6,8 +6,8 @@ sed -i "/compile/s/[0-9]*\.[0-9]*\.[0-9]*/$VERSION/" ./README.md
 
 REPOSITORY="git@github.com:${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git"
 
-git config --global user.name "CircleCI"
-git config --global user.email "app.nakayama@gmail.com"
+git config --global user.name "SebastienVancon"
+git config --global user.email "sebastien.vancon@orange.com"
 
 git add ./README.md
 git commit -m "update version [ci skip]"
