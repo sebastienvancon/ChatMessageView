1a28caca9671947b146b232f45127ae9e6b1bc46
Sébastien Vançon <sebastien.vancon@orange.com>
2018-06-21 14:06:50 +0200

    Add of the corresponding views

---
+++ chatmessageview/src/main/kotlin/com/github/bassaer/chatmessageview/view/ContactImageView.kt
@@ -0,0 +1,36 @@
+package com.github.bassaer.chatmessageview.view
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Path
+import android.graphics.RectF
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import com.github.bassaer.chatmessageview.R
+
+class ContactImageView : FrameLayout {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun onDraw(canvas : Canvas){
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            setLayerType(View.LAYER_TYPE_SOFTWARE, null)
+        }
+        var path : Path = Path();
+        val rect = RectF(0f, 0f, width.toFloat(), height.toFloat())
+        val radius = resources.getDimensionPixelSize(R.dimen.view_radius_normal).toFloat()
+        path.addRoundRect(rect,radius, radius, Path.Direction.CW)
+        canvas.clipPath(path)
+        super.onDraw(canvas)
+    }
+
+}
